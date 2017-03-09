a3b3102db9e767c07774927bc6afa9e1a2aad6cd
Miriam Rozenbaum <miriamrozenbaum@Miriams-MacBook-Pro.local>
2017-03-09 12:30:04 -0600

    finished! With all requirements

--- javascripts/inventory.js
+++ javascripts/inventory.js
@@ -43,79 +43,9 @@ var privateInventory = [];
         return privateInventory;
       }
   }
-// return privateInventory;
-// CarLot.loadInventory();
-// '()' This is the Immediatly Invoked part of IIFE
+
 })(CarLot || {}); 
 
-// CarLot.loadInventory();
-// CarLot.activateEvents();
-
-
-
-
-
-
-
-// NOTES AND QUESTIONS //
-
-// 'loadInventory' is a 'method'
-
-// Setters let dev's to modify the value of private array aka "privateInventory"
-
-// When we call loadInventory, we actually want to get the "privateInventory"
-
-      
-// Getter function? Allows us to get a private variable, and expose it to other dev's
-
-// This console log shows how we get the private variable out of the function : 
-// console.log("privateInventory:", CarLot.loadInventory());
-
-// privateInventory can not be defined in the global scope because it is a private variable of CarLot :
-// console.log("privateInventory:", privateInventory);
-
-
-
-
-
-
-///////////////////// OLD CODE //////////////////////////////////////
-
-//  "use strict";
-
-// var CarLot = (function () {
-// var privateInventory = [];
-
-
-// return {
-//   loadInventory: function (inventory) {
-//     console.log("inventory", inventory);
-//   // Create an XHR to load inventory
-//   var loader = new XMLHttpRequest();
-//   // Listen for when the lead event is broadcast
-//   // and execute an anonymous callback
-//   loader.addEventListener("load", function () {
-//     // Set the value of the private array
-//     privateInventory = JSON.parse(this.responseText).cars;
-//     console.log("privateInventory", privateInventory);
-    
-//     inventory(privateInventory);
-//     inventory(activateEvents);
-//     });
-
-//   // get the array from JSON file
-//   loader.open("GET","javascripts/inventory.json");
-//   // start the process
-//   loader.send();
-
-//   }
-
-// }
-// // return privateInventory;
-// })(CarLot || {});
-
-//  CarLot.loadInventory(showInventory);
-//  CarLot.loadInventory(activateEvents);
 
 
 
--- javascripts/styling.js
+++ javascripts/styling.js
@@ -7,12 +7,6 @@ var CarLot = (function(oldCarLot) {
 // private inventory becomes inventory
     oldCarLot.showInventory = function(inventory) {
 
-      // Array for giving car cards individual images
-      // var images = ["images/nissan-rogue-2014.jpg", "images/honda-civic-2012.jpg", "images/toyota-tocoma-2010.jpg", "images/acura-tl-2012.jpg", "images/lexus-is250-2015.jpg"]
-      // // Looping over the car images array
-      // for (var i = 0; i < images.length; i++) {
-      // // console.log("attach Images:", images[i]);
-      // }
       // List the car inventory in the DOM
       var grid = document.getElementById("carGrid");
       var outputString = "";
@@ -28,7 +22,7 @@ var CarLot = (function(oldCarLot) {
 
         `<div class="col-md-3" id="${currentInventory.make}${currentInventory.model}${currentInventory.year}">
 
-          <img class="carImg" alt="${currentInventory.make}${currentInventory.model}${currentInventory.year}">
+          <img src="${currentInventory.image}" class="carImg" alt="${currentInventory.make}${currentInventory.model}${currentInventory.year}">
 
           <p>
             Make: ${currentInventory.make}
@@ -53,90 +47,21 @@ var CarLot = (function(oldCarLot) {
       CarLot.activateEvents();
     },
       oldCarLot.styleCarCard = function(event){
-        console.log("made it to styling", event);
-        var target = event.currentTarget;
-        // $(target).addClass("bordered");
-        target.classList.add("bordered");
-        console.log("target class list:",target.classList);
-        console.log("target:", target);
-        if (target.classList.index[2]) {
-        
-          target.classList.remove("bordered");
-          console.log("target ifff:", target);
-          console.log("target class list 222:", target.classList);
-        //   console.log("target two:", target);
-        //   // $(target).removeClass("bordered");
-        }
-        
-      }
+        // Grabbing all car cards with class name
+        var carCards = document.getElementsByClassName("col-md-3");
+        // Setting current card to be current target
+        var selectedCarCard = event.currentTarget;
+          for (var n = 0; n < carCards.length; n++){
+                carCards[n].classList.remove("bordered");
+          };
+              selectedCarCard.classList.add("bordered");
+              // Focusing on search bar when specific card it clicked
+              var textInput = document.getElementById("searchBar").focus(); 
+      };
 
-      // Returning oldCarLot object plus what we added to it in showInventory    
+
       return oldCarLot;
 
 
 })(CarLot || {});
 
-
-
-//////////////////////////// OLD CODE //////////////////////////////////////
-
-// "use strict";
-
-// function showInventory (inventory){
-
-//   // Array for giving car cards individual images
-//   var images = ["images/nissan-rogue-2014.jpg", "images/honda-civic-2012.jpg", "images/toyota-tocoma-2010.jpg", "images/acura-tl-2012.jpg", "images/lexus-is250-2015.jpg"]
-//   // Looping over the car images array
-//   for (var i = 0; i < images.length; i++) {
-//     // console.log("attach Images:", images[i]);
-//   }
-//   // List the car inventory in the DOM
-//   var grid = document.getElementById("carGrid");
-//   var outputString = "";
-//   var n = 1;
-//   // Adding new DIV element 
-//   var newDiv = document.createElement("DIV");
-//   newDiv.classList.add("row");
-//     // Looping over the car inventory 
-//     for (var i = 0; i < inventory.length; i++) {
-//       var currentInventory = inventory[i];
-//       // Build up our DOM string
-//       outputString = `
-//       <div class="col-md-3" id="${currentInventory.make}${currentInventory.model}${currentInventory.year}">
-//       <img class="carImg" src="${images[i]}" alt="${currentInventory.make}${currentInventory.model}${currentInventory.year}">
-//       <p>Make: ${currentInventory.make}<br> 
-//       Model: ${currentInventory.model}<br> 
-//       Year: ${currentInventory.year}<br>
-//       <p> Description: ${currentInventory.description}</p>
-//       </div>`;  
-
-//         newDiv.innerHTML += outputString;
-  
-//         grid.appendChild(newDiv)
-//           if ( i === (n * 3) -1) {
-//             var newDiv = document.createElement("DIV");
-//             newDiv.classList.add("row");
-//             n++;
-//           }
-//     }
-
-//   activateEvents();
-// };
-
-// // Adding event listeners to car cards
-// function activateEvents (){
-//   $(".col-md-3").click(function(event){
-//     console.log("event", event.currentTarget);
-//   });
-// };
-
-  // change border color of card on and off click
-  // focus on search bar when specific car is clicked
-  // look up event.currentTarget
-
-  
-    
-
-
-
-  
